f705d39927a8eb8ef6c7c92bad264bc3546bcceb
lucky733 <luckyoffice733@gmail.com>
2025-04-10 14:34:16 +0530

    javascript

---
+++ one.js
@@ -0,0 +1,53 @@
+var a =10;
+var b= 20;
+var st1 ="hello";
+var b1 = true;
+var pi=3.142;
+var ch ='a';
+var empId;
+var sid,sname;
+//var 123PaymentAddress;
+
+document.writeln('a variable value '+a);
+document.writeln('<br>')
+document.writeln('b variable is : '+b);
+document.writeln("<br>");
+document.writeln(typeof(a));
+document.writeln("<br>" +typeof(st1));
+document.writeln("<br>" +typeof(b1));
+document.writeln("<br>" +typeof(pi));
+document.writeln("<br>" +typeof(ch));
+document.writeln("<br>")
+document.writeln("empid Id : "+empId);
+document.writeln("<br>")
+var s1="hello";
+var s2="world";
+
+document.writeln("full string is : "+s1+s2);
+document.writeln("<br>")
+var v1=10;
+var v2=20;
+document.writeln(v1+v2);
+
+var compayName;//declared
+
+compayName="HCL TECH";
+document.writeln("<br>")
+document.writeln(compayName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
