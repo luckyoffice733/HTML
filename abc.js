1225213dc76f798c2d35021dfb7ee2bb1765a493
lucky733 <luckyoffice733@gmail.com>
2025-04-10 15:56:31 +0530

    javascript

---
+++ abc.js
@@ -0,0 +1,19 @@
+var empName="smith";
+var empSal=3000;
+var a =10;
+
+document.writeln("empName  empSal")
+document.writeln("<br>")
+document.writeln(empName+" "+empSal)
+
+document.writeln("<hr>")
+document.writeln("<table border='2px'>")
+document.writeln("<tr>")
+document.writeln("<th>empName</th>")    
+document.writeln("<th>empSal</th>")    
+document.writeln("</tr>")   
+document.writeln("<tr>")
+document.writeln("<td>"+empName+"</td>")    
+document.writeln("<td>"+empSal+"</td>")     
+document.writeln("</tr>")  
+document.writeln("</table>")
