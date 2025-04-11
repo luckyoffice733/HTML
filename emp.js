b2940a4fd338277811588a777678c1eaab563f64
lucky733 <luckyoffice733@gmail.com>
2025-04-11 17:08:05 +0530

    employee Register

---
+++ emp.js
@@ -0,0 +1,7 @@
+function empDetails(){
+   
+  var fname=  document.getElementById("fn").value;
+   var lname = document.getElementById("ln").value;
+
+   document.getElementById("emp").innerHTML=fname+" "+lname;
+}
